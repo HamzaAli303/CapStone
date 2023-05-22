e2babccccb95563104c7da29d499c9688003e85d
HamzaAli303 <alihamza.m1999@gmail.com>
2023-05-22 15:44:50 -0600

    capstone

---
+++ server.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors');
+const app = express();
+const { SERVER_PORT } = process.env
+const { addPlace, getCountries, createCity, getCity, deleteCity } = require('./controller')
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+//logic
+// const db = require('./db.json');
+
+app.post('/addToList', addPlace);
+
+app.get('/countries', getCountries);
+
+app.post('/createCity', createCity);
+app.get('/createCity', getCity);
+
+app.delete('/createCity/:id', deleteCity);
+
+
+app.listen(SERVER_PORT, () => console.log('Listening on port 4003...')); 
---
+++ source.js
@@ -0,0 +1,119 @@
+console.log('hello World');
+
+let message = document.querySelector('#message');
+// let form = document.querySelector('form');
+let addToList = document.querySelector("#addToListBtn");
+let inputDate = document.querySelector('#input-date');
+let inputDestination = document.querySelector('#input-destination');
+let countrySelect = document.querySelector('#country-select');
+let viewList = document.querySelector('#viewList');
+let thirdPage = document.querySelector('#third-Page');
+
+
+let addPlace = (event) => {
+    event.preventDefault()
+
+    if (inputDestination.value < 1 && countrySelect.value === '') {
+        alert('You must enter a city and country')
+        return
+    }
+
+    console.log(inputDate.value, inputDestination.value)
+
+    let body = {
+        date: inputDate.value,
+        dest: inputDestination.value,
+        country: +countrySelect.value
+    }
+
+    axios.post('http://127.0.0.1:4003/createCity', body).then((res) => {
+        console.log(res)
+        countrySelect.value = 1
+        inputDestination.value = ''
+
+        window.location.href = `#third-Page`
+
+        // getCity();
+    }).catch(err => console.log('err'));
+
+};
+
+let getCountries = () => {
+    axios.get('http://127.0.0.1:4003/countries')
+        .then(res => {
+            console.log(res)
+            res.data.forEach(country => {
+                const option = document.createElement('option')
+                option.setAttribute('value', country['country_id'])
+                option.textContent = country.name
+                countrySelect.appendChild(option)
+            })
+        });
+};
+
+let getCity = () => {
+    console.log('here')
+    message.innerHTML = '';
+
+    axios.get('http://127.0.0.1:4003/createCity')
+        .then(res => {
+            res.data.forEach(elem => {
+                let countryCard = document.createElement(`div`)
+                countryCard.classList.add('country-card')
+                countryCard.innerHTML = `
+                <div class="countryCard">
+                <h2>${elem.city}, ${elem.country}</h2>
+                <h3>Date of travel: ${elem.date}</h3>
+                <button id="deleteBtn" onclick="deleteCard(${elem['city_id']})">Delete</button>
+                <button onclick="crossedCard(${elem['city_id']})">Visted</button></div>`
+
+
+                message.appendChild(countryCard);
+            });
+        });
+};
+
+let deleteCard = (id) => {
+    axios.delete(`http://127.0.0.1:4003/createCity/${id}`)
+        .then(res => {
+            if (res.status === 200) {
+                getCity();
+            }
+        })
+        .catch(err => console.log(err));
+};
+
+let crossedCard = (id) => {
+    var countryCard = document.getElementsByClassName('countryCard')
+    countryCard[id - 1].style.textDecoration = 'line-through'
+}
+
+
+
+
+// getCity();
+getCountries();
+addToList.addEventListener('click', addPlace);
+viewList.addEventListener('click', getCity);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
